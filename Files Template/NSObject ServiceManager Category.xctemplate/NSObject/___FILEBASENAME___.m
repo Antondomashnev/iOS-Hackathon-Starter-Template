7a9b1c147d1cbc29bdc16f349908ca2a18b5dfe8
Anton Domashnev <antondomashnev@gmail.com>
2014-01-25 18:34:28 +0400

    Update NSObject ServiceManager Category.xctemplate;

--- Files Template/NSObject ServiceManager Category.xctemplate/NSObject/___FILEBASENAME___.m
+++ Files Template/NSObject ServiceManager Category.xctemplate/NSObject/___FILEBASENAME___.m
@@ -13,20 +13,29 @@
 
 #pragma mark - ___VARIABLE_categoryName:identifier___MappableObjectProtocol
 
-static RKEntityMapping *inviteMappingWithoutRelationships;
-+ (RKEntityMapping *)CQ_entityMappingWithoutRelationshipsInStore:(RKManagedObjectStore *)store{
-    
-    if(!inviteMappingWithoutRelationships){
-        
-        inviteMappingWithoutRelationships = [RKEntityMapping mappingForEntityForName:@"CQInvite" inManagedObjectStore: store];
-        RKEntityMapping *parentMapping = [[self superclass] CQ_entityMappingWithoutRelationshipsInStore:store];
-        [inviteMappingWithoutRelationships addAttributeMappingsFromArray:[[NSArray alloc] initWithArray:parentMapping.attributeMappings copyItems:YES]];
-        [inviteMappingWithoutRelationships addAttributeMappingsFromDictionary:@{
-                                                                                @"created_at" : @"time"}];
-        [inviteMappingWithoutRelationships setIdentificationAttributes: @[CirqleDefaultPrimaryKeyAtribute]];
+static RKObjectMapping *objectMappingWithoutRelationships = nil;
++ (RKObjectMapping *)<# protocol prefix #>_objectMappingWithoutRelationships
+{
+    if(!objectMappingWithoutRelationships)
+    {
+        objectMappingWithoutRelationships = [RKObjectMapping mappingForClass:[___VARIABLE_categoryClass:identifier___ class]];
+        [objectMappingWithoutRelationships addAttributeMappingsFromDictionary:<# Mapping dictionary #>];
     }
-    
-    return inviteMappingWithoutRelationships;
+    return objectMappingWithoutRelationships;
 }
+
+static RKObjectMapping *objectMappingComplete = nil;
++ (RKObjectMapping *)<# protocol prefix #>_completeObjectMapping
+{
+    if(!objectMappingComplete)
+    {
+        objectMappingComplete = [self <# protocol prefix #>_objectMappingWithoutRelationships];
+        RKRelationshipMapping *<# relationship mapping name #> = [RKRelationshipMapping relationshipMappingFromKeyPath:@"<# key path #>" toKeyPath:@"<# key path #>" withMapping:<# relationship mapping #>];
+        [objectMappingComplete addPropertyMapping:<# relationship mapping name #>];
+    }
+    return objectMappingComplete;
+}
+
+
 
 @end
