9339d020b66d90d56810183a3267c4a146bacd94
Anton Domashnev <antondomashnev@gmail.com>
2014-02-04 22:45:22 +0400

    Update Twitter template and Facebook template;

--- Files Template/NSObject ServiceManager Category.xctemplate/NSObject/___FILEBASENAME___.h
+++ Files Template/NSObject ServiceManager Category.xctemplate/NSObject/___FILEBASENAME___.h
@@ -9,8 +9,7 @@
 ___IMPORTHEADER_categoryClass___
 #import "___VARIABLE_categoryName:identifier___DownloadableObjectProtocol.h"
 #import "___VARIABLE_categoryName:identifier___PostableObjectProtocol.h"
-#import "___VARIABLE_categoryName:identifier___MappableObjectProtocol.h"
 
-@interface ___VARIABLE_categoryClass:identifier___ (___VARIABLE_categoryName:identifier___) <___VARIABLE_categoryName:identifier___DownloadableObjectProtocol, ___VARIABLE_categoryName:identifier___PostableObjectProtocol, ___VARIABLE_categoryName:identifier___MappableObjectProtocol>
+@interface ___VARIABLE_categoryClass:identifier___ (___VARIABLE_categoryName:identifier___) <___VARIABLE_categoryName:identifier___DownloadableObjectProtocol, ___VARIABLE_categoryName:identifier___PostableObjectProtocol>
 
 @end
